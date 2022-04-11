a3ce014216b7760748ae60293c794cfef2732ef8
rengzhengcodes <rzhengcontacts@gmail.com>
2022-04-11 19:03:43 -0400

    cleanup

--- HW/Least Square Analysis/analysis.py
+++ HW/Least Square Analysis/analysis.py
@@ -12,11 +12,11 @@ penguins = penguins.loc[penguins['sex'] == 'male']
 # only data collected in 2009
 penguins = penguins.loc[penguins['year'] == 2009]
 
-print(penguins)
+# print(penguins)
 
 # selects data of bill_length, flipper length, body mass
 data = pd.DataFrame(penguins, columns=['bill_length_mm', 'flipper_length_mm', 'body_mass_g'])
-print(data.to_csv())
+# print(data.to_csv())
 # converts to numpy array
 matrix = pd.DataFrame(penguins, columns=['bill_length_mm', 'flipper_length_mm']).to_numpy()
 # adds constant values
@@ -24,8 +24,8 @@ matrix = np.hstack((matrix, [[1] for i in range(matrix.shape[0])]))
 # gets resultant vector
 result_vector = pd.DataFrame(penguins, columns=['body_mass_g']).to_numpy()
 
-print(matrix)
-print(result_vector)
+# print(matrix)
+# print(result_vector)
 
 # gets values for the importance of bill length, flipper length, and the constant value
 end = np.linalg.inv(matrix.transpose()@matrix)@matrix.transpose()@result_vector
