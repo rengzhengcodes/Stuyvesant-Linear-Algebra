a72fa139c543bb73089b4a75cd996705865029d5
rengzhengcodes <rzhengcontacts@gmail.com>
2022-04-11 19:02:38 -0400

    done

--- HW/Least Square Analysis/analysis.py
+++ HW/Least Square Analysis/analysis.py
@@ -1,5 +1,6 @@
 from palmerpenguins import load_penguins
 import pandas as pd
+import numpy as np
 # drops island info
 penguins = load_penguins()
 # filters https://www.geeksforgeeks.org/selecting-rows-in-pandas-dataframe-based-on-conditions/ for only Torgersen penguins
@@ -15,4 +16,17 @@ print(penguins)
 
 # selects data of bill_length, flipper length, body mass
 data = pd.DataFrame(penguins, columns=['bill_length_mm', 'flipper_length_mm', 'body_mass_g'])
-print(data)
+print(data.to_csv())
+# converts to numpy array
+matrix = pd.DataFrame(penguins, columns=['bill_length_mm', 'flipper_length_mm']).to_numpy()
+# adds constant values
+matrix = np.hstack((matrix, [[1] for i in range(matrix.shape[0])]))
+# gets resultant vector
+result_vector = pd.DataFrame(penguins, columns=['body_mass_g']).to_numpy()
+
+print(matrix)
+print(result_vector)
+
+# gets values for the importance of bill length, flipper length, and the constant value
+end = np.linalg.inv(matrix.transpose()@matrix)@matrix.transpose()@result_vector
+print(end)
