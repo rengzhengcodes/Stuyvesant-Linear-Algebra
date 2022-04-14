320741c89aeaadc67813cc9ad8cea5feaddcb7e4
rengzhengcodes <rzhengcontacts@gmail.com>
2022-04-14 12:50:13 -0400

    moved stuff to test file

--- HW/Second Semester Project/MNIST/lin_reg.py
+++ HW/Second Semester Project/MNIST/lin_reg.py
@@ -1,17 +1,11 @@
 ## https://www.askpython.com/python/examples/load-and-plot-mnist-dataset-in-python
-from keras.datasets import mnist
 import numpy as np
-# loads data from tuples
-(train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 
 def collapse_3D_2D(m3D:np.ndarray) -> np.ndarray:
 	if m3D.ndim == 3:
 		return m3D.reshape((m3D.shape[0], 28 * 28))
 	else:
 		raise ValueError("Expected 3D array, got " + m3D.ndim)
-
-train_X = collapse_3D_2D(train_X)
-print(train_X)
 
 from matplotlib import pyplot
 def print_data():
--- HW/Second Semester Project/MNIST/test.py
+++ HW/Second Semester Project/MNIST/test.py
@@ -15,3 +15,11 @@ def collapse_3D_2D_manual(m3D:np.ndarray) -> np.ndarray:
 		return np.asarray(arr)
 	else:
 		raise ValueError("Expected 3D array, got " + m3D.dim)
+
+# loads data from tuples
+from keras.datasets import mnist
+(train_X, train_Y), (test_X, test_Y) = mnist.load_data()
+train_X_lin = lin_reg.collapse_3D_2D(train_X)
+train_X_man = collapse_3D_2D_manual(train_X)
+
+print("3D to 2D: " + train_X_lin == train_X_man)
