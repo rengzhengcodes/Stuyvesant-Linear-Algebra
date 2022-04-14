a9287bb5f10d372de0c1c56a7d15309f01a64a5d
rengzhengcodes <rzhengcontacts@gmail.com>
2022-04-14 13:14:21 -0400

    test passed (I think)

--- HW/Second Semester Project/MNIST/test.py
+++ HW/Second Semester Project/MNIST/test.py
@@ -1,16 +1,17 @@
 import lin_reg
+import numpy as np
 
 def collapse_3D_2D_manual(m3D:np.ndarray) -> np.ndarray:
 	if m3D.ndim == 3:
 		arr = list()
 		m3D = m3D.tolist()
 
-		for row in n3D:
-			row = list()
+		for row in m3D:
+			row_new = list()
 			for column in row:
 				for height in column:
-					row.append(height)
-			arr.append(list)
+					row_new.append(height)
+			arr.append(row_new)
 
 		return np.asarray(arr)
 	else:
@@ -20,6 +21,11 @@ def collapse_3D_2D_manual(m3D:np.ndarray) -> np.ndarray:
 from keras.datasets import mnist
 (train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 train_X_lin = lin_reg.collapse_3D_2D(train_X)
+np.savetxt("foo.csv", train_X_lin, delimiter=",")
+print(train_X_lin)
+(train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 train_X_man = collapse_3D_2D_manual(train_X)
+print(train_X_man)
+np.savetxt("bar.csv", train_X_man, delimiter=",")
 
-print("3D to 2D: " + train_X_lin == train_X_man)
+print("3D to 2D: " + str(train_X_lin == train_X_man))
