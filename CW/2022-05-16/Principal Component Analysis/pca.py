71c54014b6114e060f53b7a4a453f451401b09ec
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-16 12:53:59 -0400

    imports

--- CW/2022-05-16/Principal Component Analysis/pca.py
+++ CW/2022-05-16/Principal Component Analysis/pca.py
@@ -1,1 +1,14 @@
+"""
+imports data
+"""
 import csv
+
+file = csv.reader(open("2D_data.txt"))
+file = list(file)
+# str to integers
+file = [[float(x), float(y)] for x, y in file]
+
+"""
+converts to matrix
+"""
+import numpy as np
