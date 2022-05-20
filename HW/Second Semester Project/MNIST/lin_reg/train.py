b66c733d5b7febffa3e3131abe272667353dbd0a
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-20 12:57:09 -0400

    fixed output

--- HW/Second Semester Project/MNIST/lin_reg/train.py
+++ HW/Second Semester Project/MNIST/lin_reg/train.py
@@ -3,4 +3,4 @@ from keras.datasets import mnist
 (train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 
 result = lin_reg.regress2D_matrix(lin_reg.collapse_3D_2D(train_X), train_Y.transpose())
-result.savetxt("res.csv", result, delimiter=',')
+np.savetxt("res.csv", result, delimiter=',')
