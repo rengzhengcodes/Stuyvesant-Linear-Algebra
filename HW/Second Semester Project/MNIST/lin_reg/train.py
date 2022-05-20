1e83e19dc0283c96a0dcbb154c0a4af9e8189476
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-20 13:54:59 -0400

    normalized the results (980 9020)

--- HW/Second Semester Project/MNIST/lin_reg/train.py
+++ HW/Second Semester Project/MNIST/lin_reg/train.py
@@ -4,4 +4,6 @@ from keras.datasets import mnist
 (train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 
 result = lin_reg.regress2D_matrix(lin_reg.collapse_3D_2D(train_X), train_Y.transpose())
+norm = np.linalg.norm(result)
+result = result / norm
 np.savetxt("res.csv", result, delimiter=',')
