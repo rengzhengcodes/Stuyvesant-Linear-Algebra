40d827e5cfd2b452f43d4d2ab4197f5445d6a7b7
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-24 13:11:16 -0400

    divide training data by 255

--- HW/Second Semester Project/MNIST/lin_reg/train.py
+++ HW/Second Semester Project/MNIST/lin_reg/train.py
@@ -3,6 +3,8 @@ import numpy as np
 from keras.datasets import mnist
 (train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 
+train_X = train_X / 255
+
 result = lin_reg.regress2D_matrix(lin_reg.collapse_3D_2D(train_X), train_Y.transpose())
 norm = np.linalg.norm(result)
 result = result / norm
