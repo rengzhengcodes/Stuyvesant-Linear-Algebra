4666ae5a95cc58287e3f213d5106bcde4f6a92cf
rengzhengcodes <rzhengcontacts@gmail.com>
2022-06-01 12:58:14 -0400

    train and matrix

--- HW/Second Semester Project/MNIST/addition/train.py
+++ HW/Second Semester Project/MNIST/addition/train.py
@@ -1,11 +1,22 @@
 import numpy as np
+import json
 from keras.datasets import mnist
 (train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 
 matrices = dict()
 nums = set(train_Y)
 for item in nums:
-	matrices[item] = np.zeros((28, 28), dtype=float)
+	matrices[int(item)] = np.zeros((28, 28), dtype=float)
 
 for i in range(len(train_Y)):
-	matrices[train_Y[i]] += train_X[i]
+	matrices[train_Y[i]] += train_X[i] / 255
+
+for key, value in matrices.items():
+	matrices[key] = value / train_Y.tolist().count(key)
+
+for key, value in matrices.items():
+	matrices[key] = value.tolist()
+
+print(matrices)
+
+json.dump(matrices, open("matrices.json", "w"))
