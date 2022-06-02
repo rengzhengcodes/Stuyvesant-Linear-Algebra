5a05368167b255043a899357aa0508699ef94a22
rengzhengcodes <rzhengcontacts@gmail.com>
2022-06-02 12:49:06 -0400

    0 success!

---
+++ HW/Second Semester Project/MNIST/addition/check.py
@@ -0,0 +1,47 @@
+import numpy as np
+import json
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+(train_X, train_Y), (test_X, test_Y) = mnist.load_data()
+
+matrices = json.load(open("matrices.json"))
+
+"""
+for key, value in matrices.items():
+	plt.matshow(value)
+
+plt.show()
+"""
+
+
+for key, value in matrices.items():
+	# print(value)
+	matrices[key] = np.array(value)
+	# print(matrices[key])
+	matrices[key] = np.reshape(matrices[key], (28*28))
+	# print(matrices[key])
+
+correct = 0
+incorrect = 0
+
+for i in range(len(test_Y)):
+	results = dict()
+	max_score = 0
+	best_val = None
+	vector = np.reshape(test_X[i], (28 * 28))
+	vector = vector / np.linalg.norm(vector)
+
+	for key, value in matrices.items():
+		results[key] = value.dot(vector)
+		if results[key] > max_score:
+			max_score = results[key]
+			best_val = key
+
+	if best_val == test_Y[i]:
+		correct += 1
+	else:
+		print(i)
+		print(results)
+		incorrect += 1
+
+print(correct, incorrect)
