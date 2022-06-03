0d975b6f7a2abef92bfb90c0734da61471e73551
rengzhengcodes <rzhengcontacts@gmail.com>
2022-06-03 13:02:28 -0400

    starting off with ideal, one pass 4099 5901

--- HW/Second Semester Project/MNIST/dynamic_addition_and_subtraction/train.py
+++ HW/Second Semester Project/MNIST/dynamic_addition_and_subtraction/train.py
@@ -3,35 +3,35 @@ import json
 from keras.datasets import mnist
 (train_X, train_Y), (test_X, test_Y) = mnist.load_data()
 
+# aggregated all data
+temp = json.load(open("start.json"))
 matrices = dict()
-nums = set(train_Y)
-for item in nums:
-	matrices[int(item)] = np.zeros((28, 28), dtype=float)
 
-for i in range(len(train_Y)):
-	max_score = 0
-	best_val = None
-	vector = np.reshape(train_X[i], (28 * 28))
-	vector = vector / np.linalg.norm(vector)
-	results = dict()
+for key, value in temp.items():
+	matrices[int(key)] = value
 
-	for key, value in matrices.items():
-		results[key] = value.reshape((28 * 28)).dot(vector)
-		if results[key] > max_score:
-			max_score = results[key]
-			best_val = key
+del temp
+print(matrices)
+for i in range(1):
+	for i in range(len(train_Y)):
+		max_score = 0
+		best_val = 0
+		vector = np.reshape(train_X[i], (28 * 28))
+		vector = vector / np.linalg.norm(vector)
+		results = dict()
 
-	if best_val == train_Y[i]:
-		# incentivizes keeping this argument
-		matrices[best_val] += train_X[i]
-	else:
-		# removes errors from others
-		matrices[train_Y[i]] += train_X[i]
-		nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
-		nums.remove(train_Y[i])
+		for key, value in matrices.items():
+			results[key] = np.reshape(value, (28 * 28)).dot(vector)
+			if results[key] > max_score:
+				max_score = results[key]
+				best_val = key
 
-		for num in nums:
-			matrices[num] -= train_X[i]
+		if best_val == train_Y[i]:
+			# incentivizes keeping this argument
+			matrices[best_val] += train_X[i]
+		else:
+			# removes errors from that guess
+			matrices[best_val] -= train_X[i]
 
 for key, value in matrices.items():
 	matrices[key] = value / np.linalg.norm(value)
